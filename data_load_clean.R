78b531609eda720bd63756e08d57d713fd9b06dd
ddunkelmann <74114818+ddunkelmann@users.noreply.github.com>
2020-12-26 14:43:52 +0100

    Init Copy
    Inital Copy of the existing Skripts and Data

---
+++ data_load_clean.R
@@ -0,0 +1,211 @@
+# Libraries
+library(tidyverse)
+library(readr)
+
+# ------------------------- #
+# Mario Kart 8 Charakter and Combinations Analysis
+# ------------------------- #
+
+
+# 1. Functions ####
+# -- Rewriting all Columns to Lowercase
+
+all_col_toLower <- function(df) {
+  for(n in 1:ncol(df)) {
+    colnames(df)[n] <- tolower(colnames(df)[n])
+  }
+  
+  return(df)
+}
+
+# 2. Loading the Data ####
+# _ 2.1 Characters ####
+
+# Load Data
+csv_characters <- read_csv("Data/characters.csv")
+view(csv_characters)
+
+# Clean the Dataframe (different Speed and Handling Values are weighted approxamitely to their share in each track)
+clean_characters <- csv_characters %>% 
+  mutate(Speed = `Speed (Ground)` * 0.8 + `Speed (Air)` * 0.1 + `Speed (Water)` *0.1,
+         Handling = `Handling(Ground)` * 0.8 + `Handling (Air)` * 0.1 + `Handling (Water)` *0.1) %>% 
+  select(-c(starts_with("Speed ("), starts_with("Handling ("), "Handling")) %>% 
+  rename("mini_turbo" = "Mini Turbo",
+         "handling" = "Handling(Ground)")
+
+clean_characters <- all_col_toLower(clean_characters)
+
+# DF giving identical Character "Types"
+unique_characters <- clean_characters %>% 
+  select(-character) %>% 
+  unique() %>% # Testing for duplicates (based on values)
+  mutate(paste_id = paste0(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% # ID for the matching (next Step)
+  select(paste_id)
+
+# Giving each individual group a name
+unique_characters$group <- c("Light_1", "Light_2", "Medium_1", "Medium_2", "Heavy_1", "Heavy_2", "Heavy_3")
+
+# Join the unique Groups to the DF
+characters <- clean_characters %>% 
+  mutate(paste_id = paste0(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_"),
+         class = factor(class, levels = c("Light", "Medium", "Heavy"))) %>% 
+  left_join(unique_characters, by = "paste_id") %>% 
+  select(-paste_id) %>% 
+  mutate(group = factor(group, levels = c("Light_1", "Light_2", "Medium_1", "Medium_2", "Heavy_1", "Heavy_2", "Heavy_3")))
+
+# Clean the Workspace
+rm(clean_characters, csv_characters)
+# _ 2.2 Karts ####
+
+# Identical method as before. Tables have small differences which are individually corrected.
+csv_bodies <- read_csv("Data/bodies.csv")
+
+clean_bodies <- csv_bodies %>% 
+  rename("mini_turbo" = "Mini Turbo",
+         "handling" = "Handling")
+
+clean_bodies <- all_col_toLower(clean_bodies)
+
+unique_bodies_comb <- clean_bodies %>% 
+  select(-vehicle) %>% 
+  unique() %>% 
+  mutate(paste_id = paste(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% 
+  select(paste_id)
+
+# Giving Groups ID, not Names since their classification is not as easy as for the characters
+unique_bodies_comb$comb_id <- 1:18
+
+bodies <- clean_bodies %>% 
+  mutate(paste_id = paste(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% 
+  left_join(unique_bodies_comb, by = "paste_id") %>% 
+  select(-paste_id)
+
+rm(csv_bodies, clean_bodies)
+
+# _ 2.3 Tires ####
+
+csv_tires <- read_csv("Data/tires.csv")
+
+clean_tires <- csv_tires %>% 
+  mutate(Speed = `Speed (Ground)` * 0.8 + `Speed (Air)` * 0.1 + `Speed (Water)` *0.1,
+         Handling = `Handling(Ground)` * 0.8 + `Handling (Air)` * 0.1 + `Handling (Water)` *0.1) %>% 
+  select(-contains("(")) %>% 
+  rename("mini_turbo" = "Mini Turbo",
+         "handling" = "Handling")
+
+clean_tires <- all_col_toLower(clean_tires)
+
+unique_tires_comb <- clean_tires %>% 
+  select(-body) %>% 
+  unique() %>% 
+  mutate(paste_id = paste(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% 
+  select(paste_id)
+
+unique_tires_comb$comb_id <- 1:7
+
+tires <- clean_tires %>% 
+  mutate(paste_id = paste(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% 
+  left_join(unique_tires_comb, by = "paste_id") %>% 
+  select(-paste_id)
+
+rm(csv_tires, clean_tires)
+
+# _ 2.4 Glider ####
+
+csv_gliders <- read_csv("Data/gliders.csv")
+
+clean_gliders <- csv_gliders %>% 
+  mutate(Speed = `Speed (Ground)` * 0.8 + `Speed (Air)` * 0.1 + `Speed (Water)` *0.1,
+         Handling = `Handling(Ground)` * 0.8 + `Handling (Air)` * 0.1 + `Handling (Water)` *0.1) %>% 
+  select(-contains("(")) %>% 
+  rename("mini_turbo" = "Mini Turbo",
+         "handling" = "Handling")
+
+clean_gliders <- all_col_toLower(clean_gliders)
+
+unique_gliders_comb <- clean_gliders %>% 
+  select(-c(body, type)) %>% 
+  unique() %>% 
+  mutate(paste_id = paste(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% 
+  select(paste_id)
+
+unique_gliders_comb$comb_id <- 1:2
+
+
+gliders <- clean_gliders %>% 
+  mutate(paste_id = paste(speed, acceleration, weight, handling, traction, mini_turbo, sep = "_")) %>% 
+  left_join(unique_gliders_comb, by = "paste_id") %>% 
+  select(-paste_id)
+
+rm(csv_gliders, clean_gliders)
+
+# 3. Creating all Combinations through crossing() ####
+
+# Creating all Combinations based on their Groud IDs --> 1764 Combinations
+cross_df <- crossing(char = unique_characters$group, 
+                                 bodies = unique_bodies_comb$comb_id,
+                                 tires = unique_tires_comb$comb_id,
+                                 glider = unique_gliders_comb$comb_id) %>% 
+  
+  # Joining the remaining information to the combinations above
+  # Cars
+  left_join(bodies, by = c("bodies" = "comb_id")) %>% 
+  select(-vehicle) %>% 
+  unique() %>% 
+  
+  # Characters
+  left_join(characters, by = c("char" = "group"), suffix = c("_body", "_char")) %>% 
+  select(-c(character, class)) %>% 
+  unique() %>% 
+  
+  # Tires
+  left_join(tires, by = c("tires" = "comb_id")) %>% 
+  select(-body) %>% 
+  unique() %>% 
+  
+  # Gliders
+  left_join(gliders, by = c("glider" = "comb_id"), suffix = c("_tires", "_glider")) %>% 
+  select(-c(type, body)) %>% 
+  unique() %>% 
+  
+  # Creating aggregated values for the main stats
+  mutate(speed_sum = speed_body + speed_char + speed_tires + speed_glider,
+         acceleration_sum = acceleration_body + acceleration_char +acceleration_tires + acceleration_glider,
+         weight_sum = weight_body + weight_char + weight_tires + weight_glider,
+         handling_sum = handling_body + handling_char + handling_tires + handling_glider,
+         traction_sum = traction_body + traction_char + traction_tires + traction_glider,
+         mini_turbo_sum = mini_turbo_body + mini_turbo_char + mini_turbo_tires + mini_turbo_glider) %>% 
+  
+  # Removing the single columns of each part
+  select(-c(ends_with("_body"), ends_with("_char"), ends_with("_tires"), ends_with("_glider")))
+
+view(cross_df)
+
+rm(unique_bodies_comb, unique_characters, unique_tires_comb, unique_gliders_comb)
+# 4. Creating a formula for weighted evaluation of the combinations. #####
+
+# The custom scores are based on personal preference. 
+# A player might prefer a fast combination over a combination with good handling and control. 
+# Even though there is a clear pattern of which archetypes are present within the game, A player can prefer speed and handling. 
+# The data will try and give him a a score based on his values
+cross_df_archetype <- cross_df %>% 
+  mutate(
+        # Archetype Levels are a numeric value, describing how much a certain combination represents one of the archetypes. 
+         # -1 represents the small, control type, whereas 1 represents a fast and heavy combination
+         # Version 1 is based on true values
+         archetype_lvl = (0 - (acceleration_sum + handling_sum + traction_sum + mini_turbo_sum) / 4
+                           + (speed_sum + weight_sum) / 2) / 4,
+         # Version 2 is based on the difference to the mean (average)
+         archetype_lvl_mean = (0 - ((acceleration_sum - mean(acceleration_sum)) + 
+                                    (handling_sum  - mean(handling_sum)) +
+                                    (traction_sum - mean(handling_sum)) +
+                                    (mini_turbo_sum - mean(mini_turbo_sum))) / 4 
+                                 + ((speed_sum - mean(speed_sum)) +
+                                    (weight_sum - mean(weight_sum))) / 2) /4)
+  
+rm(cross_df)
+
+
+
+
+# . #####
---
+++ visual_plots.R
@@ -0,0 +1,229 @@
+# Libraries
+library(plotly)
+library(ggplot2)
+library(viridisLite)
+library(GGally)
+
+# ------------------ #
+# Mario Kart Analysis Visuals
+# ------------------ #
+
+# 1. Correlation within the different building parts ####
+
+# _ 1.1 Characters ####
+
+# Creating a ggplot-friendly DF to build a heatmap for the specific attributes
+plot_character_class <- characters %>% 
+  group_by(group) %>% 
+  summarize(speed = mean(speed),
+            acceleration = mean(acceleration),
+            weight = mean(weight),
+            handling = mean(handling),
+            traction = mean(traction),
+            mini_turbo = mean(mini_turbo)
+  ) %>% 
+  gather("attribut", "mean", -group) %>% 
+  mutate(attribut = factor(attribut, levels = c("weight", "speed", "acceleration", "mini_turbo", "traction", "handling")))
+
+# Used when creating the Heatmap for each character instead of the groups
+         # character = factor(character, levels = c("Baby Mario", "Baby Luigi","Baby Peach","Baby Daisy","Baby Rosalina","Lemmy Koopa","Mii Light",    
+         #                                          "Toad"          ,  "Shy Guy"       ,  "Koopa Troopa" ,   "Lakitu"     ,     "Wendy Koopa"    , "Larry Koopa"  ,   "Toadette" ,      
+         #                                          "Peach"      ,     "Daisy"     ,      "Yoshi"       ,    "Mario"       ,    "Luigi"     ,      "Iggy Koopa"   ,   "Ludwig Koopa" ,  
+         #                                          "Mii Medium"  ,    "Donkey Kong"  ,   "Waluigi"     ,    "Rosalina"    ,    "Roy Koopa"    ,   "Metal Mario"   ,  "Pink Gold Peach",
+         #                                          "Wario"     ,      "Bowser"     ,     "Morton Koopa"  ,  "Mii Heavy" )))
+
+# Heatmap of the different Charactergroups and their values
+ggplot(plot_character_class, aes(x = attribut, y = group)) +
+  geom_tile(aes(fill = mean)) +
+  geom_text(aes(label = mean)) +
+  
+  scale_fill_gradientn(colours = viridis(256, option = "D")) +
+  theme_classic() +
+  theme(
+    axis.line.y = element_blank(),
+    axis.line.x = element_blank(),
+    legend.position = "bottom"
+  ) +
+  labs(title = "Attributes Heatmap", subtitle = "Characters", y = NULL, x = NULL)
+
+# The Heatmap shows, that there is a clear correlation between the 6 different Attributes. 
+# These being: 
+# - Weight and Speed
+# - Acceleration, Mini Turbo, Traction, Handling
+
+# Attributes within a group increase together, whereas attributes from different groups do not (if one increases, the other group decreases)
+# This means, that you can pick two different types of characters: Heavy and Fast or Small with good Handling
+
+# _ 1.2 Cars ####
+# Same Method as above
+# The difference of the cars (and other parts next to characters) is that they modify (increase or decrease) the base values given by the Character. 
+# The changes are lot smaller and include negative Values
+
+plot_bodies_class <- bodies %>% 
+  group_by(comb_id) %>% 
+  summarize(speed = mean(speed),
+            acceleration = mean(acceleration),
+            weight = mean(weight),
+            handling = mean(handling),
+            traction = mean(traction),
+            mini_turbo = mean(mini_turbo)
+  ) %>% 
+  mutate(sum_diff = speed + acceleration + weight + handling + traction + mini_turbo) %>% 
+  gather("attribut", "mean", -c(comb_id, sum_diff)) %>% 
+  mutate(attribut = factor(attribut, levels = c("weight", "speed", "acceleration", "mini_turbo", "traction", "handling")))
+
+# Heatmap
+ggplot(plot_bodies_class, aes(x = attribut, y = reorder(comb_id, desc(sum_diff)))) +
+  geom_tile(aes(fill = mean)) +
+  geom_text(aes(label = mean)) +
+
+  # Different Colorscale to highlight positive/negative values
+  scale_fill_gradient2(high = "#fde725", low = "#440154", mid = "white") +
+  theme_classic() +
+  theme(
+    legend.position = "bottom",
+    axis.line.y = element_blank(),
+    axis.line.x = element_blank()
+  ) +
+  labs(title = "Attributes Heatmap", subtitle = "Cars", y = NULL, x = NULL)
+
+
+
+# _ 1.3 Wheels ####
+plot_tires_class <- tires %>% 
+  select(-body) %>% 
+  unique() %>% 
+  mutate(sum_diff = speed + acceleration + weight + handling + traction + mini_turbo) %>% 
+  gather("attribut", "mean", -c(comb_id, sum_diff)) %>% 
+  mutate(attribut = factor(attribut, levels = c("weight", "speed", "acceleration", "mini_turbo", "traction", "handling")))
+
+ggplot(plot_tires_class, aes(x = attribut, y = reorder(comb_id, sum_diff))) +
+  geom_tile(aes(fill = mean)) +
+  geom_text(aes(label = mean)) +
+  
+  scale_fill_gradient2(high = "#fde725", low = "#440154", mid = "white") +
+  theme_classic() +
+  theme(
+    legend.position = "bottom",
+    axis.line.y = element_blank(),
+    axis.line.x = element_blank()
+  ) +
+  labs(title = "Attributes Heatmap", subtitle = "Wheels", y = NULL, x = NULL)
+
+
+# _ 1.4 Gliders ####
+plot_gliders_class <- gliders %>% 
+  select(-c(body, type)) %>% 
+  unique() %>% 
+  mutate(sum_diff = speed + acceleration + weight + handling + traction + mini_turbo) %>% 
+  gather("attribut", "mean", -c(comb_id, sum_diff)) %>% 
+  mutate(attribut = factor(attribut, levels = c("weight", "speed", "acceleration", "mini_turbo", "traction", "handling")))
+
+ggplot(plot_gliders_class, aes(x = attribut, y = reorder(comb_id, sum_diff))) +
+  geom_tile(aes(fill = mean)) +
+  geom_text(aes(label = mean)) +
+  
+  scale_fill_gradient2(high = "#fde725", low = "#440154", mid = "white") +
+  theme_classic() +
+  theme(
+    legend.position = "bottom",
+    axis.line.y = element_blank(),
+    axis.line.x = element_blank()
+  ) +
+  labs(title = "Attributes Heatmap", subtitle = "Glider", y = NULL, x = NULL)
+
+# _ 1.5 Takeaways ####
+# There is a correlation throughout all of the different parts in a setup. 
+# As described above there are two groups of attributes, correlation with each other:
+# - Weight and Speed
+# - Acceleration, Mini Turbo, Traction, Handling
+
+# Attributes within a group increase together, whereas attributes from different groups do not (if one increases, the other group decreases)
+# This means, that you can pick two different types of characters: Heavy and Fast or Small with good Handling
+
+# Scatter für unterschiedliche Kombinationen
+# Testen eines multiplen Korrelations diagramm
+
+
+# 2. Correlation Matrix ####
+# Using the function ggpairs() to create a correlatio Matrix of the 6 Attributes available
+GGally::ggpairs(cross_df_archetype, columns = 5:10)
+
+# As the visual shows are all of the correlation significant.
+# The strongest positive cor. are:
+# - Weight x Speed (+0.89)
+# - Acceleration x Mini Turbo (+0.822)
+
+# The strongest negative cor. are:
+# - Speed x Acceleration (-0.802)
+# - Handling x Weight (-0.772)
+
+# This information gives proof to the ideas of archetypes above
+
+# Saving
+ggsave("Plots/cor_matrix.pdf", width = 15, height = 15)
+
+
+# 3. Visualizing Scores ####
+# This visualization wants to show how the 1700 combinations are spread across the two archetypes.
+# It shows, that more combinations are leaning towards the heavy and fast type, 
+#  this could be due to the 3 heavy charactersgroups existing (in contrary to only two small charactergroups)
+
+# Histogram for all scores
+ggplot(cross_df_archetype, aes(x = archetype_lvl)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous(limits = c(-1.25, 1.25)) +
+  theme_minimal() +
+  labs(title = "Histogram of Archetype Scores", subtitle = "-1 = Handling and Accel., 1 = Heavy and Speed", x = "Score", y = NULL)
+
+
+# 4. Further Analysis ####
+
+# Inside of the data_load_clean.R a user could change the scoring to his personal preferences. 
+# The DF could get them give him the best setup for his preferences, which can then be found on our -1 to 1 scale. 
+# Different testings resulted in various results. This method would lean towards one of the archetypes but would not give the most extreme values. 
+
+# Function that gives a DF with the 3 best Scores
+give_personal_best <- function(speed_score, acceleration_score, 
+                               weight_score, handling_score, 
+                               traction_score, mini_turbo_score) {
+  personal_score <- cross_df_archetype %>% 
+    mutate(
+      custom_score = speed_sum * speed_score + 
+        acceleration_sum * acceleration_score +
+        weight_sum * weight_score + 
+        handling_sum * handling_score + 
+        traction_sum * traction_score +
+        mini_turbo_sum * mini_turbo_score
+    ) %>% 
+    slice_max(custom_score, n = 3)
+  
+  return(personal_score)
+}
+
+# Example
+personal_score <- give_personal_best(2, 1, 1, 0.5, 5, 1)
+
+tibble(personal_score)
+
+# Drawing the best combination into the histogram above
+ggplot(cross_df_archetype, aes(x = archetype_lvl)) +
+  geom_histogram(bins = 50) +
+  scale_x_continuous(limits = c(-1.25, 1.25)) +
+  theme_minimal() +
+  labs(title = "Histogram of Archetype Scores", subtitle = "-1 = Handling and Accel., 1 = Heavy and Speed", x = "Score", y = NULL) +
+  
+  geom_vline(data = personal_score, aes(xintercept = archetype_lvl, alpha = archetype_lvl), color = "green") +
+  theme(legend.position = "none")
+
+
+# 5. What's next? ####
+
+# Another idea would be to test all of these setups in real races. 
+# Then the player himself/herself could change the scoring based on his experiences.
+# With that, a player could slowly iterate towards his "perfect" Setup. 
+# However, no "one best setup" could be identified, rather a knowledge of the archetypes can be given.
+
+
+# . #####
+
